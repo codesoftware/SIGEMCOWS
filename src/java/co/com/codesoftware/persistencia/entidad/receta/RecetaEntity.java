1bd409f4891d95b6d91d47d619af831545711d2c
jmorenor1986 <johmorenoing@gmail.com>
2016-04-17 10:49:30 -0500

    dfg

--- src/java/co/com/codesoftware/persistencia/entidad/receta/RecetaEntity.java
+++ src/java/co/com/codesoftware/persistencia/entidad/receta/RecetaEntity.java
@@ -6,9 +6,7 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
